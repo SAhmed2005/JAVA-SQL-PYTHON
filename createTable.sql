8bede43099536ec8520a7e493bc0e8d53dd85aea
SAhmed2005 <samiahmed1705@gmail.com>
2025-06-15 20:33:26 -0400

    Create createTable.sql

---
+++ createTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    hire_date DATE,
+    salary DECIMAL(10, 2)
+);
